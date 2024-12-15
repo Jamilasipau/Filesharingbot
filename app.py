29f8370a0ea3bcafc12ac878d6aaac9b291374c2
Jamilasipau <jamilasipai+vybnb@gmail.com>
2024-12-15 17:17:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'GreyMatters'
+def hello():
+    return "Hello, world!"
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
